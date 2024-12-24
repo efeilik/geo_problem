fd9843acc480257097b23b0f46395fed289cb613
Efeİlik <efe.ilik@ug.bilkent.edu.tr>
2024-12-24 17:28:31 +0000

    initial commit

---
+++ src/main/java/com/weatherapp/geo_spring/GeoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.weatherapp.geo_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weatherapp/geo_spring/GeoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weatherapp.geo_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
